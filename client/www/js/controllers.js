046555b4e6a349a5425c0891d37e179bf4d4fa3d
Kiran Rao <k.ranrao@gmail.com>
2015-01-24 17:52:41 -0800

    Adds additional features to client.  Dummy jaunt data now included in client.
    Map lat and lon switched to San Francisco
    
    Adds nested state example
    
    Adjusts to dummy icons for search and map
    
    Converts search to unstyled mvp design
    
    Adds nav bar - currently buggy
    
    Comment cleanup
    
    Fixes search and map icons
    
    Adjusts to more flexible template
    
    Index comment cleanup
    
    Tab icon edits
    
    Removes template friends and friends details
    
    Refactored chats to reference jaunts throughout app
    
    Reduced dummy data from 5 items to 3 items
    
    Adds dummy place data into dummy jaunt data
    
    Removes deleted method from controller
    
    Adds method to jaunts factory for jaunt places data
    
    Adds img data to places for testing
    
    Changes jaunt text property to description
    
    Completes jaunts to jaunt to jaunt place flow
    
    Changes reference from dashbaord to search
    
    Adds additional data to jaunts and begins styling
    
    Removes search tab and adds fancy popover to map
    
    Begins setting up jaunt popover on map
    
    Renames myApp as client

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,11 +1,14 @@
 angular.module('starter.controllers', [])
 
-/*to be adjusted*/
-.controller('SearchCtrl', function($scope) {})
-
-.controller('MapCtrl', function($scope, $ionicLoading) {
+.controller('MapCtrl', function($scope, $ionicLoading, $ionicPopover) {
   $scope.mapCreated = function(map) {
     $scope.map = map;
+
+    // note: created a dummy point that on click will show popover of jaunt
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(37.7833, -122.4167),
+      map: $scope.map
+    });
   };
 
   $scope.centerOnMe = function () {
@@ -27,10 +30,50 @@ angular.module('starter.controllers', [])
       alert('Unable to get location: ' + error.message);
     });
   };
+
+  // adds popover functionality to map.  Need to connect to map listener
+  $ionicPopover.fromTemplateUrl('templates/popover.html', {
+    scope: $scope,
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+  /*$scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });*/
 })
 
-/*to be adjusted*/
-.controller('JauntCtrl', function($scope) {})
+.controller('JauntsCtrl', function($scope, Jaunts) {
+  $scope.jaunts = Jaunts.allJaunts();
+})
 
-/*to be adjusted*/
-.controller('AccountCtrl', function($scope) {});
+.controller('JauntDetailCtrl', function($scope, $stateParams, Jaunts) {
+  $scope.jaunt = Jaunts.getJaunt($stateParams.jauntId);
+  $scope.places = Jaunts.allPlaces($scope.jaunt);
+  $scope.tags = Jaunts.allTags($scope.jaunt);
+})
+
+.controller('PlaceDetailCtrl', function($scope, $stateParams, Jaunts) {
+  $scope.place = Jaunts.getPlace($stateParams.jauntId, $stateParams.placeId)
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
