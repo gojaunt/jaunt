7f9eb6713d1cb4c3d39b409a87682c1c0f08dc23
zdlopez <zachary.d.lopez@gmail.com>
2015-01-28 11:00:37 -0800

    Adds polylines to services.  Adds click functionality to map controller, displays and removes maps per selection.
    Adds polylines to services.  Adds click functionality to map controller, displays and removes maps per selection.
    
    tested geoLocation

--- client/www/js/services.js
+++ client/www/js/services.js
@@ -14,6 +14,27 @@ angular.module('starter.services', [])
     tags: ['Beer', 'Drunk', 'Sights'],
     description: 'Descriptive hipster lorem forage retro lo-fi hashtag food truck Blue Bottle. Organic wolf Pinterest, crucifix trust fund Wes Anderson leggings twee tote bag +1.',
     face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',
+    steps: [
+      {
+        start_location : {
+          coordinates : [-122.4088122, 37.7834319]
+        },
+        end_location: {
+          coordinates : [-122.4085524, 37.7836379]
+        }
+
+      },
+      {
+        start_location : {
+          coordinates : [-122.4085524, 37.7836379]
+        },
+        end_location: {
+          coordinates : [-122.4086873, 37.7844507]
+        }
+
+      }
+
+    ],
     places: [{
       id: 0,
       name: 'Bar1',
@@ -37,6 +58,26 @@ angular.module('starter.services', [])
     tags: ['Beer', 'Drunk', 'Sights'],
     description: 'Descriptive hipster lorem forage retro lo-fi hashtag food truck Blue Bottle. Organic wolf Pinterest, crucifix trust fund Wes Anderson leggings twee tote bag +1.',
     face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460',
+    steps : [
+     {
+        start_location : {
+          coordinates : [-122.4123220999998, 37.7824969]
+        },
+        end_location: {
+          coordinates : [-122.4124236999998, 37.7830119]
+        }
+
+      },
+      {
+        start_location : {
+          coordinates : [-122.4124236999998, 37.7830119]
+        },
+        end_location: {
+          coordinates : [-122.41392000000002, 37.7828141]
+        }
+
+      }
+      ],
     places: [{
       id: 0,
       name: 'Bar2',
@@ -60,6 +101,7 @@ angular.module('starter.services', [])
     tags: ['Beer', 'Drunk', 'Sights'],
     description: 'Descriptive hipster lorem forage retro lo-fi hashtag food truck Blue Bottle. Organic wolf Pinterest, crucifix trust fund Wes Anderson leggings twee tote bag +1.',
     face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg',
+    steps : [],
     places: [{
       id: 0,
       name: 'Bar3',
@@ -110,6 +152,24 @@ angular.module('starter.services', [])
         }
       }
       return null;
+    },
+    getAllPolys : function(){
+      var polys = [];
+      
+      for(var i = 0; i< jaunts.length; i++){
+        var linePoints = [];
+        for(var j = 0; j < jaunts[i].steps.length; j++){
+          var startPoint = new google.maps.LatLng(jaunts[i].steps[j].start_location.coordinates[1], jaunts[i].steps[j].start_location.coordinates[0]);
+          var endPoint = new google.maps.LatLng(jaunts[i].steps[j].end_location.coordinates[1], jaunts[i].steps[j].end_location.coordinates[0]);
+          linePoints.push(startPoint);
+          linePoints.push(endPoint);
+
+        }
+        var poly = new google.maps.Polyline({strokeColor: 'blue'});
+        poly.setPath(linePoints);
+        polys.push(poly);
+      }
+      return polys;  
     }
   }
 });
